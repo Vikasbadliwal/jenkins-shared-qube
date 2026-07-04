269bd20ae27966c631e15fdbb037dd81711c30ba
vikasbadliwal <badliwalvikash@gmail.com>
2026-07-04 23:00:28 +0530

    fix: pass private key to ansible for final destination

--- vars/configureSonar.groovy
+++ vars/configureSonar.groovy
@@ -1,11 +1,13 @@
 def call(String bastionIp) {
     echo "Establishing secure tunnel to Bastion at ${bastionIp}..."
     
-    // Notice StrictHostKeyChecking=no is now at the very front AND inside the ProxyCommand
     def sshArgs = "-o StrictHostKeyChecking=no -o ProxyCommand=\"ssh -W %h:%p -q ubuntu@${bastionIp} -i sonarkey.pem -o StrictHostKeyChecking=no\""
     
+    // Added --private-key and -u ubuntu so Ansible can authenticate with the private server
     sh """
         ansible-playbook -i ansible/aws_ec2.yml ansible/site.yml \
+        --private-key sonarkey.pem \
+        -u ubuntu \
         --ssh-common-args='${sshArgs}'
     """
 }
