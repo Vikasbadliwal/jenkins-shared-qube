3c62c4002ca219222b6ffe47360a73f03562d372
vikasbadliwal <badliwalvikash@gmail.com>
2026-07-04 23:36:59 +0530

    fix: dynamically pass RDS endpoint and credentials to ansible

--- vars/configureSonar.groovy
+++ vars/configureSonar.groovy
@@ -1,13 +1,19 @@
 def call(String bastionIp) {
-    echo "Establishing secure tunnel to Bastion at ${bastionIp}..."
+    echo "Extracting AWS RDS Endpoint and establishing secure tunnel..."
+    
+    // Extract the exact RDS URL dynamically from Terraform's outputs
+    def rdsEndpoint = sh(script: 'terraform output -raw rds_endpoint', returnStdout: true).trim()
     
     def sshArgs = "-o StrictHostKeyChecking=no -o ProxyCommand=\"ssh -W %h:%p -q ubuntu@${bastionIp} -i sonarkey.pem -o StrictHostKeyChecking=no\""
     
-    // Added --private-key and -u ubuntu so Ansible can authenticate with the private server
+    // Inject the endpoint and credentials directly into Ansible
     sh """
         ansible-playbook -i ansible/aws_ec2.yml ansible/site.yml \
         --private-key sonarkey.pem \
         -u ubuntu \
+        -e "db_endpoint=${rdsEndpoint}" \
+        -e "sonarqube_db_user=sonar" \
+        -e "sonarqube_db_password=YourSecureDBPassword123" \
         --ssh-common-args='${sshArgs}'
     """
 }
