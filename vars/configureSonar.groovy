70f9feeb0b51ba3c7f5e44e85b61fdd0acc31c22
vikasbadliwal <badliwalvikash@gmail.com>
2026-07-04 21:54:00 +0530

    fix: point pipeline to site.yml playbook

--- vars/configureSonar.groovy
+++ vars/configureSonar.groovy
@@ -1,13 +1,12 @@
-// vars/configureSonar.groovy
 def call(String bastionIp) {
     echo "Establishing secure tunnel to Bastion at ${bastionIp}..."
-
+    
     // Build the dynamic SSH proxy command
     def sshArgs = "-o ProxyCommand=\"ssh -W %h:%p -q ubuntu@${bastionIp} -i sonarkey.pem -o StrictHostKeyChecking=no\""
-
-    // Execute the Ansible playbook
+    
+    // Execute the Ansible playbook using site.yml
     sh """
-        ansible-playbook -i ansible/aws_ec2.yml ansible/deploy-sonarqube.yml \
+        ansible-playbook -i ansible/aws_ec2.yml ansible/site.yml \
         --ssh-common-args='${sshArgs}'
     """
 }
