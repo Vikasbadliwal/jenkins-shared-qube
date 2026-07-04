3b28fbfc4bbb9bf0bed8d8c040d05b9ec1bc5e09
vikasbadliwal <badliwalvikash@gmail.com>
2026-07-04 22:45:13 +0530

    fix: disable host key checking for ansible destination

--- vars/configureSonar.groovy
+++ vars/configureSonar.groovy
@@ -1,10 +1,9 @@
 def call(String bastionIp) {
     echo "Establishing secure tunnel to Bastion at ${bastionIp}..."
     
-    // Build the dynamic SSH proxy command
-    def sshArgs = "-o ProxyCommand=\"ssh -W %h:%p -q ubuntu@${bastionIp} -i sonarkey.pem -o StrictHostKeyChecking=no\""
+    // Notice StrictHostKeyChecking=no is now at the very front AND inside the ProxyCommand
+    def sshArgs = "-o StrictHostKeyChecking=no -o ProxyCommand=\"ssh -W %h:%p -q ubuntu@${bastionIp} -i sonarkey.pem -o StrictHostKeyChecking=no\""
     
-    // Execute the Ansible playbook using site.yml
     sh """
         ansible-playbook -i ansible/aws_ec2.yml ansible/site.yml \
         --ssh-common-args='${sshArgs}'
