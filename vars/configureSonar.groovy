2a4a0ad8012aee06244035568b6b6c52a848e4b6
vikasbadliwal <badliwalvikash@gmail.com>
2026-07-08 13:24:43 +0530

    fix: New updated file

--- vars/configureSonar.groovy
+++ vars/configureSonar.groovy
@@ -1,19 +1,20 @@
 def call(String bastionIp) {
-    echo "Extracting AWS RDS Endpoint and establishing secure tunnel..."
+    echo "Extracting AWS RDS Endpoint and preparing SSH Keys..."
     
-    // Extract the exact RDS URL dynamically from Terraform's outputs
+    // 1. Prepare the SSH key securely in the background
+    sh 'cp $SSH_KEY_PATH sonarkey.pem'
+    sh 'chmod 400 sonarkey.pem'
+
     def rdsEndpoint = sh(script: 'terraform output -raw rds_endpoint', returnStdout: true).trim()
-    
     def sshArgs = "-o StrictHostKeyChecking=no -o ProxyCommand=\"ssh -W %h:%p -q ubuntu@${bastionIp} -i sonarkey.pem -o StrictHostKeyChecking=no\""
     
-    // Inject the endpoint and credentials directly into Ansible
     sh """
         ansible-playbook -i ansible/aws_ec2.yml ansible/site.yml \
         --private-key sonarkey.pem \
         -u ubuntu \
         -e "db_endpoint=${rdsEndpoint}" \
         -e "sonarqube_db_user=sonar" \
-        -e "sonarqube_db_password=YourSecureDBPassword123" \
+        -e "sonarqube_db_password=\$TF_VAR_db_password" \
         --ssh-common-args='${sshArgs}'
     """
 }
